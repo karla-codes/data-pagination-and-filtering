275899d923f6be91d22085fc4ec11d2473a06553
karla-walker <indiablonix@gmail.com>
2021-04-27 03:11:48 -0700

    Added function that creates and appends pagination buttons

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
 function showPage(list, page) {
   const startIndex = page * 9 - 9;
   const endIndex = page * 9;
@@ -39,11 +38,36 @@ function showPage(list, page) {
   }
 }
 
-showPage(data, 1);
-
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+function addPagination(list) {
+  const paginationBtns = Math.ceil(list.length / 9);
+  const ul = document.querySelector('.link-list');
+  ul.innerHTML = '';
 
+  for (let i = 0; i < paginationBtns; i++) {
+    const buttonHTML = `
+      <li>
+        <button type="button">${i + 1}</button>
+      </li>
+    `;
+    ul.insertAdjacentHTML('beforeend', buttonHTML);
+  }
+
+  const firstButton = document.querySelector('button');
+  firstButton.className = 'active';
+
+  ul.addEventListener('click', e => {
+    const activeButton = document.querySelector('.active');
+    const clickedButton = e.target;
+    activeButton.className = '';
+    clickedButton.className = 'active';
+    showPage(data, clickedButton.textContent);
+  });
+}
+
+showPage(data, 1);
 // Call functions
+addPagination(data);
